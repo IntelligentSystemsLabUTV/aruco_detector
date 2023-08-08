820f29ce007873706dd84c773bfdc8d7dafcc81c
Lorenzo Bianchi <lnz.bnc@gmail.com>
2023-08-08 18:34:59 +0200

    chore: modify launch and config file

--- src/aruco_detector/launch/aruco_detector_seppia.launch.py
+++ src/aruco_detector/launch/aruco_detector_seppia.launch.py
@@ -12,7 +12,9 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
@@ -20,23 +22,36 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # Build config file path
-    config_file = os.path.join(
+    config = os.path.join(
         get_package_share_directory('aruco_detector'),
         'config',
-        'aruco_detector.yaml'
+        'config.yaml'
     )
+
+    # Declare launch arguments
+    ns = LaunchConfiguration('namespace')
+    cf = LaunchConfiguration('cf')
+    ns_launch_arg = DeclareLaunchArgument(
+        'namespace',
+        default_value='seppia'
+    )
+    cf_launch_arg = DeclareLaunchArgument(
+        'cf',
+        default_value=config
+    )
+    ld.add_action(ns_launch_arg)
+    ld.add_action(cf_launch_arg)
 
     # Create node launch description
     node = Node(
         package='aruco_detector',
         executable='aruco_detector',
+        namespace=ns,
         exec_name='aruco_detector_app',
-        # namespace='seppia',
-        shell=True,
         emulate_tty=True,
         output='both',
         log_cmd=True,
-        parameters=[config_file]
+        parameters=[cf]
     )
 
     ld.add_action(node)
